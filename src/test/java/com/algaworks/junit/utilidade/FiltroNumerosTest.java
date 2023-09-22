d103b4ff0c560f7aed239cb31ec838a39e0fc133
Fábio Araujo Gauna <f703054@corp.caixa.gov.br>
2023-09-21 21:09:36 -0300

    Asserções em lista

---
+++ src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
@@ -0,0 +1,20 @@
+package com.algaworks.junit.utilidade;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class FiltroNumerosTest {
+
+	@Test
+	public void deveRetornarNumerosPares() {
+		List<Integer> numeros = Arrays.asList(1, 2, 3, 4);
+		List<Integer> numerosParesEsperados = Arrays.asList(2, 4);
+		List<Integer> numerosFiltrados = FiltroNumeros.numerosPares(numeros);
+		assertIterableEquals(numerosParesEsperados, numerosFiltrados);
+	}
+
+}
