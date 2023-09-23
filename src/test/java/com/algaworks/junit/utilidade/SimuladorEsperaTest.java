70aa1bbf84cb3b1cc5f2b2f930b400ea697f1fea
Fábio Araujo Gauna <f703054@corp.caixa.gov.br>
2023-09-23 09:46:05 -0300

    Desabilitando testes unitários

--- src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -4,11 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
 import java.time.Duration;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class SimuladorEsperaTest {
 
 	@Test
+	@Disabled("Não é mais necessário")
 	public void deveEsperarENaoDarTimeout() {
 		assertTimeoutPreemptively(Duration.ofSeconds(1), () -> SimuladorEspera.esperar(Duration.ofMillis(10)));
 	}
