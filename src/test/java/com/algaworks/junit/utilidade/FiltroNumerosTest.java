532a9bf06356d4decb4853c6d8dc5a9ae15c4f15
Fábio Araujo Gauna <f703054@corp.caixa.gov.br>
2023-09-23 13:03:12 -0300

    Organizando classe de testes com sub-classes e @Nested

--- src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
+++ src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class FiltroNumerosTest {
@@ -17,6 +18,34 @@ class FiltroNumerosTest {
 		List<Integer> numerosFiltrados = FiltroNumeros.numerosPares(numeros);
 		
 		assertIterableEquals(numerosParesEsperados, numerosFiltrados);
+	}
+	
+	@Test
+	public void deveRetornarNumerosImpares() {
+		List<Integer> numeros = Arrays.asList(1, 2, 3, 4);
+		List<Integer> numerosImparesEsperados = Arrays.asList(1, 3);
+		
+		List<Integer> numerosFiltrados = FiltroNumeros.numerosImpares(numeros);
+		
+		assertIterableEquals(numerosImparesEsperados, numerosFiltrados);
+	}
+	
+	@Test
+	void deveVerificarSeUmNumeroEPositivo() {
+		int numero = 1;
+		
+		boolean isPositivo = FiltroNumeros.isPositivo(numero);
+		
+		Assertions.assertTrue(isPositivo);
+	}
+	
+	@Test
+	void deveVerificarSeUmNumeroNaoPositivo() {
+		int numero = -1;
+		
+		boolean isPositivo = FiltroNumeros.isPositivo(numero);
+		
+		Assertions.assertFalse(isPositivo);
 	}
 
 }
